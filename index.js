022b292ac37639d2d0c11617fba9a0ece4c37ab8
Sophie Lucas <slucas2376@gmail.com>
2020-11-23 20:16:50 -0500

    cors troubleshooting

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.use(expressSession({
     secret: "coronavirus really needs to just Not(tm)",
     resave: false,
     saveUninitialized: false,
-    // proxy: true, // true for heroku
+    proxy: true, // true for heroku
     cookie: {sameSite: "none", // comment out for local testing
         secure: true, // false for local testing
         maxAge: 5184000000
