754af6ae57870d3348ace6ce936e97d779332d5e
Raj Gandecha <rajg@ad.unc.edu>
2020-11-24 13:37:32 -0500

    changes

--- server.js
+++ server.js
@@ -110,6 +110,7 @@ app.post('/register', (req, res) => {
         return;
     }
     let u = User.create(userId, displayName, password, avatar);
+    console.log("new user created:" + u.userId);
     res.json(true);
     return;
 })
