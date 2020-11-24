26ed75e5ff3ad03ec74aec3bcf04d10ab92ebe73
Sophie Lucas <slucas2376@gmail.com>
2020-11-23 21:17:28 -0500

    https/port troubleshooting

--- index.js
+++ index.js
@@ -462,7 +462,7 @@ app.delete('/tweets/:id', (req, res) => {
 })
 
 
-const port = process.env.port || 443;
+const port = process.env.port || 3030;
 app.listen(port, () => {
     console.log('server running on port ' + port);
     //User.createAdmin("sclu", "Dev Sophie", "426final", "https://imgur.com/572zTSG.jpg", "mood: aaaaaaaaaaaaaaa");
